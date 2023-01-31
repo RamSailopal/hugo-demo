54550efb23b189fc3e03b4aec2b1105713f3a499
Raman Sailopal <thelinuxguys@yahoo.com>
2023-01-31 11:15:00 +0000

    Update entrypoint.sh
    Gitpod and local deployment actually the same

--- data/entrypoint.sh
+++ data/entrypoint.sh
@@ -2,11 +2,9 @@
 apt-get update
 apt-get install -y hugo git
 cd /home/data/test
-if [[ "$GITPOD_HOST" != "" ]]
-then
-    git init
-    rm -Rf themes
-    git config --global --add safe.directory /home/data/test
-    git submodule add https://github.com/budparr/gohugo-theme-ananke.git themes/ananke
-fi
+git init
+rm -Rf themes
+git config --global --add safe.directory /home/data/test
+git submodule add https://github.com/budparr/gohugo-theme-ananke.git themes/ananke
 hugo server --bind 0.0.0.0
+
