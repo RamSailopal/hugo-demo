895cc2a7ff87b8fff0e208c55a0d918b3a28f4b6
Raman Sailopal <thelinuxguys@yahoo.com>
2023-01-31 11:08:37 +0000

    Create gitpodentrypoint.sh
    Adding specific Gitpod deployment file.

---
+++ data/gitpodentrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+apt-get update
+apt-get install -y hugo git
+cd /home/data/test
+git init
+rm -Rf themes
+git config --global --add safe.directory /home/data/test
+git submodule add https://github.com/budparr/gohugo-theme-ananke.git themes/ananke
+hugo server --bind 0.0.0.0
